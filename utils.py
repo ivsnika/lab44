b864f826d745f9f157d5e8568a3fa25b13f2fc5d
salukaa <krashanytsia@knu.ua>
2026-03-02 00:50:12 +0200

    Fix print statements to use double quotes

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 def ignatyeva():
-print (veronika)
+print ("veronika")
 
 def krashanytsia():
-    print(ya jelau vsem dobraaa)
+    print("ya jelau vsem dobraaa")
 
 def makaka():
-    print(meh-mat)
+    print("meh-mat")
